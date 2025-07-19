e62773103aad2122ac9cd5a717fa94d4212b9533
devs <your@gmail.com>
2025-07-19 22:39:28 +0700

    rollback

---
+++ example/multiple_clients/example_multiple.py
@@ -0,0 +1,63 @@
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+
+from pytgcalls import idle
+from pytgcalls import PyTgCalls
+from pytgcalls.types import MediaStream
+
+app = Client(
+    'py-tgcalls',
+    api_id=123456789,
+    api_hash='abcdef12345',
+)
+app2 = Client(
+    'py-tgcalls',
+    api_id=123456789,
+    api_hash='abcdef12345',
+)
+
+test_stream = 'http://docs.evostream.com/sample_content/assets/' \
+              'sintel1m720p.mp4'
+
+# You can enter an unlimited number of PyTgCalls clients
+call_py = PyTgCalls(app)
+call_py2 = PyTgCalls(app2)
+
+
+@app.on_message(filters.regex('!p1'))
+async def play_handler(_: Client, message: Message):
+    await call_py.join_group_call(
+        message.chat.id,
+        MediaStream(
+            test_stream,
+        ),
+    )
+
+
+@app.on_message(filters.regex('!p2'))
+async def play_handler2(_: Client, message: Message):
+    await call_py2.join_group_call(
+        message.chat.id,
+        MediaStream(
+            test_stream,
+        ),
+    )
+
+
+@app.on_message(filters.regex('!s1'))
+async def stop_handler(_: Client, message: Message):
+    await call_py.leave_group_call(
+        message.chat.id,
+    )
+
+
+@app.on_message(filters.regex('!s2'))
+async def stop_handler2(_: Client, message: Message):
+    await call_py2.leave_group_call(
+        message.chat.id,
+    )
+
+call_py.start()
+call_py2.start()
+idle()
---
+++ example/simple_calls/example_simple.py
@@ -0,0 +1,103 @@
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+
+from pytgcalls import idle
+from pytgcalls import PyTgCalls
+from pytgcalls.types import MediaStream
+from pytgcalls.types import Update
+
+app = Client(
+    'py-tgcalls',
+    api_id=123456789,
+    api_hash='abcdef12345',
+)
+call_py = PyTgCalls(app)
+
+test_stream = 'http://docs.evostream.com/sample_content/assets/' \
+              'sintel1m720p.mp4'
+
+
+@app.on_message(filters.regex('!play'))
+async def play_handler(_: Client, message: Message):
+    await call_py.join_group_call(
+        message.chat.id,
+        MediaStream(
+            test_stream,
+        ),
+    )
+
+
+@app.on_message(filters.regex('!change_stream'))
+async def change_handler(_: Client, message: Message):
+    await call_py.change_stream(
+        message.chat.id,
+        MediaStream(
+            test_stream,
+        ),
+    )
+
+
+@app.on_message(filters.regex('!cache'))
+async def cache_handler(_: Client, _m: Message):
+    print(call_py.cache_peer)
+
+
+@app.on_message(filters.regex('!ping'))
+async def ping_handler(_: Client, _m: Message):
+    print(call_py.ping)
+
+
+@app.on_message(filters.regex('!pause'))
+async def pause_handler(_: Client, message: Message):
+    await call_py.pause_stream(
+        message.chat.id,
+    )
+
+
+@app.on_message(filters.regex('!resume'))
+async def resume_handler(_: Client, message: Message):
+    await call_py.resume_stream(
+        message.chat.id,
+    )
+
+
+@app.on_message(filters.regex('!stop'))
+async def stop_handler(_: Client, message: Message):
+    await call_py.leave_group_call(
+        message.chat.id,
+    )
+
+
+@app.on_message(filters.regex('!change_volume'))
+async def change_volume_handler(_: Client, message: Message):
+    await call_py.change_volume_call(
+        message.chat.id,
+        50,
+    )
+
+
+@app.on_message(filters.regex('!status'))
+async def get_play_status(client: Client, message: Message):
+    await client.send_message(
+        message.chat.id,
+        f'Current seconds {await call_py.played_time(message.chat.id)}',
+    )
+
+
+@call_py.on_kicked()
+async def kicked_handler(_: PyTgCalls, chat_id: int):
+    print(f'Kicked from {chat_id}')
+
+
+@call_py.on_stream_end()
+async def stream_end_handler(_: PyTgCalls, update: Update):
+    print(f'Stream ended in {update.chat_id}', update)
+
+
+@call_py.on_participants_change()
+async def participant_handler(_: PyTgCalls, update: Update):
+    print(update)
+
+call_py.start()
+idle()
